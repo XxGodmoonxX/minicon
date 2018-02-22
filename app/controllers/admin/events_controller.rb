5b42f209d09d63376f805847bfd76e62c631d9c9
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2018-02-22 09:03:29 +0000

    fix

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,12 +1,15 @@
 class Admin::EventsController < ApplicationController
   layout 'admin'
   
+  before_action :set_event, only: [:show, :edit, :update]
+  before_action :event_params, only: [:update, :create]
+  
   def index
     @events = Event.all
   end
 
   def show
-    @event = Event.find(params[:id])
+    # @event = Event.find(params[:id])
   end
 
   def new
@@ -14,12 +17,29 @@ class Admin::EventsController < ApplicationController
   end
 
   def edit
+    # @event = Event.find(params[:id])
   end
   
   def create
-    event_params = params.require(:event).permit(:title, :location, :start_at, :ticket_price, :ticket_quantity, :event_category_id)
+    # event_params = params.require(:event).permit(:title, :location, :start_at, :ticket_price, :ticket_quantity, :event_category_id)
     @event = Event.new(event_params)
     @event.save
     redirect_to admin_event_path(@event), notice: 'Event was successfully created.'
   end
+  
+  def update
+    # event_params = params.require(:event).permit(:title, :location, :start_at, :ticket_price, :ticket_quantity, :event_category_id)
+    @event.update(event_params)
+    redirect_to admin_event_path(@event), notice: 'Event was successfullt updated.'
+  end
+  
+  private
+  
+    def set_event
+      @event = Event.find(params[:id])
+    end
+    
+    def event_params
+      params.require(:event).permit(:title, :location, :start_at, :ticket_price, :ticket_quantity, :event_category_id)
+    end
 end
